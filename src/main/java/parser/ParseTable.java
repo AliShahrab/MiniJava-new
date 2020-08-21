f19af357ae42aa522378f232ed582862ddd9a57f
ali shahrab <ali.shahrab@gmail.com>
2020-08-21 16:43:50 +0400

    IdempotentOperations

--- src/main/java/parser/ParseTable.java
+++ src/main/java/parser/ParseTable.java
@@ -24,7 +24,7 @@ public class ParseTable {
                 try {
                     nonTerminals.put(i, NonTerminal.valueOf(temp));
                 }catch (Exception e){
-                    temp = temp;
+                    throw e;
                 }
             }
             else {
